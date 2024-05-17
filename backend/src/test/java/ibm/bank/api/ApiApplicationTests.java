3896856b0c4f36f159b1941f3639df1e4ece77b6
Danilo Cassiano Dias <dcassianodias@gmail.com>
2024-05-17 16:59:02 -0300

    Primeiro commit

---
+++ backend/src/test/java/ibm/bank/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ibm.bank.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
