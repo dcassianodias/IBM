c39488deacfc2bce00e6aa5e5b5f55ff495ad8e4
Danilo Cassiano Dias <dcassianodias@gmail.com>
2024-05-20 19:25:52 -0300

    adequacao frontend

--- banco-frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ClienteComponent } from './components/cliente/cliente.component';
-
-const routes: Routes = [
-  { path: 'clientes', component: ClienteComponent },
-  // Adicione outras rotas aqui, se necessário
-];
-
-export default routes; // ou export const routes;
