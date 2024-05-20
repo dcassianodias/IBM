285dae2efc84718a6278b9618501150ba95bcdcb
Danilo Cassiano Dias <dcassianodias@gmail.com>
2024-05-20 00:21:16 -0300

    criação do frontend do projeto

---
+++ banco-frontend/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { ClienteComponent } from './components/cliente/cliente.component';
+
+export const routes: Routes = [
+  { path: '', component: ClienteComponent }
+];
